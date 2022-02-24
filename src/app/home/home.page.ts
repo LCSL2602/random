75c152d7f70ceb04d2b9299dafdb998517561426
Luis Sulbaran <luis.sulbaran@emqu.net>
2022-02-23 21:51:51 -0400

    new specifications

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -11,7 +11,8 @@ export class HomePage implements OnInit {
   public countMark:number = 0
   public time = new Date()
   public sizeScreen:number
-
+  public counterRevision:number = 0
+  public numToRandom:number = 6
   constructor() {}
 
   ngOnInit(): void {
@@ -23,23 +24,35 @@ export class HomePage implements OnInit {
     this.num = 0
   }
 
+  generateNumber():number{
+    return  Math.floor(Math.random() * (this.numToRandom - 1) + 1)
+  }
+
   add_random():void {
     this.countMark++
     this.flagMsg = false
     this.time = new Date()
-    if(this.countMark === 20){
-        this.num = 5
-        this.countMark = 0
+
+    if(this.countMark === 10){
+      this.numToRandom = 6
+      this.counterRevision = 0
+      this.countMark = 0
     }else{
-        if(this.list_addend.length == 4){
-            this.clean()
+        if(this.list_addend.length === 4){
+          this.clean()
         }
         while(this.list_addend.length < 4){
-            const numRandom = Math.floor(Math.random() * (5 - 1) + 1)
-            if(this.list_addend.indexOf(numRandom) == -1){
-                this.num = numRandom
+            const numRandom = this.generateNumber()
+            if(this.list_addend.indexOf(numRandom) === -1){
+              if(numRandom !== 5){
                 this.list_addend.push(numRandom)
-                break
+              }else{
+                this.counterRevision++
+                if(this.counterRevision === 1) 
+                  this.numToRandom = 5
+              }
+              this.num = numRandom
+              break
             }
         }
     }
